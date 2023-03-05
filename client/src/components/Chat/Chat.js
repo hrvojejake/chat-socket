465c452270a0fb202d32e2309452f7701e9df4ef
Danikuk <soloquedate@gmail.com>
2023-03-05 16:25:23 +0100

    first commit

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,32 +1,66 @@
-import React,{useState, useEffect} from 'react';
-import {queriString} from 'query-string';
-import io from 'socket.io-client';
+import React, { useState, useEffect } from "react";
+import queryString from 'query-string';
+import io from "socket.io-client";
+
+
+import InfoBar from '../infoBar/InfoBar';
+
+
+import './Chat.css';
+
+const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
 
 let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setRoom(room);
+    setName(name)
+
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
+  
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages(messages => [ ...messages, message ]);
+    });
     
-const Chat = ({location}) => {
-    const [name, setName] = useState ('');
-    const [room, setRoom] = useState ('');
-    const ENDPOINT = 'localhost:5000';
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+}, []);
 
-        
-        useEffect (() => {
-            const  {name, room} = queriString.parse(location.search);
-           
-            socket= io (ENDPOINT);
+  const sendMessage = (event) => {
+    event.preventDefault();
 
-            setName(name);
-            setRoom (room);
-            
-            socket.emit('join', {name, room});
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
 
-        }, [ENDPOINT, location.search]);
-   
-    return (
-
-        <h1>Chat</h1>
-
-    )
+  return (
+    <div className="outerContainer">
+      <div className="container">
+          <InfoBar room={room} />
+          
+      </div>
+     
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
